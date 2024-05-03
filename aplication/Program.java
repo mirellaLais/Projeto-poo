538ac35dc3957c723b3a130a1329a0d09603ffa7
Mirella Lais <oliveiramirella434@gmail.com>
2024-05-02 22:00:31 -0300

    coorigi o erro e instaciei a classe Alunos

--- aplication/Program.java
+++ aplication/Program.java
@@ -1,6 +1,8 @@
 package aplication;
 import java.util.Scanner;
 
+import entities.Aluno;
+
 public class Program {
 
     public static void main(String[] args) {
@@ -10,22 +12,23 @@ public class Program {
         System.out.println("Matricula Escolar");
         System.out.println("Por favor, insira as informações do aluno:");
         
-        System.out.print("\nNome do aluno: ");
+        System.out.print("\nNome do aluno (Completo): ");
 		String nome = sc.nextLine();
+
+        System.out.print("Idade: ");
+        int idade = sc.nextInt();
+        sc.nextLine();
 
         System.out.print("Data de nascimento (DD/MM/AAAA): ");
         String dataNascimento = sc.nextLine();
 
-        System.out.print("Idade: ");
-        int idade = sc.nextInt();
+        System.out.print("Endereço: ");
+        String endereco = sc.nextLine();
 
         System.out.print("Nome do responsável: ");
         String responsavel = sc.nextLine();
 
-        System.out.print("Endereço: ");
-        String endereco = sc.nextLine();
-
-        System.out.print("Telefone para contato: ");
+        System.out.print("Telefone para contato: ");  
         String telefone = sc.nextLine();
 
         System.out.print("Série desejada: ");
@@ -33,13 +36,15 @@ public class Program {
     
         System.out.println("\nProcessando matrícula...");
 
-        System.out.println("\nMatrícula realizada com sucesso para o/a aluno(a) " + nome);
-        System.out.println("Série: " + serie);
-        System.out.println("Data de nascimento e idade: " + dataNascimento + ", " + idade + " anos");
-        System.out.println("Endereço: " + endereco);
-        System.out.println("Responsável: " + responsavel);
-        System.out.println("Telefone de contato: " + telefone);   
+        Aluno Aluno = new Aluno(nome, dataNascimento, responsavel, endereco, telefone, idade, serie);
 
+        System.out.println("\nMatrícula realizada com sucesso para o/a aluno(a) " + Aluno.getNome());
+        System.out.println("Série: " + Aluno.getSerie(serie));
+        System.out.println("Data de nascimento e idade: " + Aluno.getDataNascimento() + ", " + Aluno.getIdade(idade) + " anos");
+        System.out.println("Endereço: " + Aluno.getEndereco());
+        System.out.println("Responsável: " + Aluno.getResponsavel());
+        System.out.println("Telefone de contato: " + Aluno.getTelefone());   
+    
     sc.close();
 
     }
